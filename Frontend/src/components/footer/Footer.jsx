4f41e9e4549e4ef51f28317d965e140b2b6b5310
Manar <75319989+manarlam@users.noreply.github.com>
2025-06-19 15:31:43 +0200

    delete_logo

--- Frontend/src/components/footer/Footer.jsx
+++ Frontend/src/components/footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../../assets/images/logo.png";
 import {
   AiFillGithub,
 } from "react-icons/ai";
