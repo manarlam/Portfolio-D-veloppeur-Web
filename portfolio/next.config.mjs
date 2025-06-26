a4b1edb9efc3e7a3fc3a60ed48eafb93f0ba8af8
Manar <75319989+manarlam@users.noreply.github.com>
2025-06-26 15:22:55 +0200

    gh_pages

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/Portfolio-Developpeur-Web',
+    trailingSlash: true,
+};
+
 
 export default nextConfig;
