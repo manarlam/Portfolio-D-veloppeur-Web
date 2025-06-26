455677acf5f8474a2869c4c99fa391602011861a
Manar <75319989+manarlam@users.noreply.github.com>
2025-06-26 11:32:45 +0200

    title

--- portfolio/components/collapse/Collapse.jsx
+++ portfolio/components/collapse/Collapse.jsx
@@ -4,7 +4,7 @@ function Collapse({ title, children, isOpen, onClick }) {
   return (
     <div className="collapse-box">
       <div className="collapse-header" onClick={onClick}>
-        <h5>{title}</h5>
+        <h3>{title}</h3>
       </div>
       <div className={`collapse-content ${isOpen ? 'visible' : ''}`}>
   {children}
